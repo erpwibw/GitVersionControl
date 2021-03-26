562331ff410f4e8a5c7031319bc99d8d4094c0f2
satday <chenchangwei92@gmail.com>
2021-03-26 21:10:31 +0800

    Initial Commit

---
+++ Shared/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  Shared
+//
+//  Created by 陈长炜 on 2021/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
